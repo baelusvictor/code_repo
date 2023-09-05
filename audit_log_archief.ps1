b960844bea0b9ca5a6de7f015685f58d53d6f8a8
baelusvictor <victor.baelus@gmail.com>
2023-09-05 09:36:34 +0200

    0

--- audit_log_archief.ps1
+++ audit_log_archief.ps1
@@ -199,3 +199,24 @@ foreach ($i in 1..14) {
     Select Id, RecordType, CreationTime, Operation, OrganizationId, UserType, UserKey, Workload, UserId, ClientIP, UserAgent, Activity, ItemName, WorkSpaceName, DatasetName, ReportName, CapacityId, CapacityName, WorkspaceId, ObjectId, DataflowId, DataflowName, AppName, DataflowAccessTokenRequestParameters, DatasetId, ReportId, IsSuccess, DataflowType, ReportType, RequestId, ActivityId, AppReportId, DistributionMethod, ConsumptionMethod |
     Export-Csv -NoTypeInformation -Path $csvFile
 }
+
+
+
+Login-PowerBI
+    #bij activatie script wordt huidige datum als input gebruikt, niet inzitten met overlap want duplicaten worden via power query verwijderd en bestanden op zelfde datum overschreven
+    #gaat twee weken terug
+    $CurrentDate = Get-Date
+    $Datum = $CurrentDate.ToString("yyyy-MM-dd")
+    #je kan enkel data downloaden van dezelfde dag, werkt enkel met DateTime, vandaar deze onhandige omweg
+    $End = $Datum+'T23:59:59'
+    $EndTime = [datetime]::ParseExact($End, "yyyy-MM-ddTHH:mm:ss", [CultureInfo]::InvariantCulture)
+    $EndTimeVar = $EndTime.AddDays(-1)
+    $EndString = $EndTimeVar.ToString("yyyy-MM-ddTHH:mm:ss")
+    $Begin = $Datum+'T00:00:00'
+    $BeginTime = [datetime]::ParseExact($Begin, "yyyy-MM-ddTHH:mm:ss", [CultureInfo]::InvariantCulture)
+    $BeginTimeVar = $BeginTime.AddDays(-1)
+    $StartString = $BeginTimeVar.ToString("yyyy-MM-ddTHH:mm:ss")
+    #$dateString = "download"+$CurrentDate.ToString("yyyy_MM_dd_HH_mm_ss")+"_datum"+$BeginTimeVar.ToString("yyyy_MM_dd")
+    $dateString = $BeginTimeVar.ToString("yyyy_MM_dd")
+    $activities = (Get-PowerBIActivityEvent -StartDateTime $StartString -EndDateTime $EndString -User victor.baelus@vrt.be -Activity RefreshDataset -ResultType JsonString |
+    ConvertFrom-Json) 
