48f2e6d2143c806b78448e9b9ab8c8efdc2f5b19
baelusvictor <victor.baelus@gmail.com>
2023-09-11 12:09:53 +0200

    0

---
+++ powershell/invoke_REST_example.ps1
@@ -0,0 +1,5 @@
+﻿#Login-PowerBI
+
+#GET https://api.powerbi.com/v1.0/myorg/admin/reports/e4806a61-3001-49a1-a3d1-d7c200d05036/users
+
+Invoke-PowerBIRestMethod -Url "https://api.powerbi.com/v1.0/myorg/admin/reports/e4806a61-3001-49a1-a3d1-d7c200d05036/users" -Method GET
---
+++ powershell/refresh_REST_example.ps1
@@ -0,0 +1,5 @@
+﻿#GET https://api.powerbi.com/v1.0/myorg/groups/f72f3129-587b-4536-872e-65398e15a28b/datasets/3058a228-9d2c-4ddc-9998-5c53aea14512/refreshes
+
+#https://app.powerbi.com/groups/f72f3129-587b-4536-872e-65398e15a28b/datasets/3058a228-9d2c-4ddc-9998-5c53aea14512/details?experience=power-bi
+
+Invoke-PowerBIRestMethod -Url "https://api.powerbi.com/v1.0/myorg/groups/f72f3129-587b-4536-872e-65398e15a28b/datasets/3058a228-9d2c-4ddc-9998-5c53aea14512/refreshes" -Method GET
