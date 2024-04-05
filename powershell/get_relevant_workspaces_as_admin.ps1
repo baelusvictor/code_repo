9ef757c2b560b9afc953624d55d917700a3106c9
victor baelus <victor.baelus@gmail.com>
2024-04-05 14:33:17 +0200

    a bit closer

---
+++ powershell/get_relevant_workspaces_as_admin.ps1
@@ -0,0 +1,7 @@
+﻿#Login-PowerBI
+
+$Workspaces_all = Get-PowerBiWorkspace -Scope Organization -All 
+$Workspaces_filtered = $Workspaces_all | Where-Object { $_.Type -ne "PersonalGroup" } | Where-Object { $_.Type -ne "Personal" }
+
+Write-Output $Workspaces_filtered.id 
+$Workspaces_filtered 
